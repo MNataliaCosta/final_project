aa2da8930c31a0172062526dadb1e155af44edbe
Natalia Costa <m.nataliascosta@gmail.com>
2019-04-03 18:19:38 -0400

    hamburger menu implemented across all pages and menu linked as separate file

---
+++ js/menu.js
@@ -0,0 +1,8 @@
+function menuClick() {
+  var menu = document.getElementById("menu-options");
+  if (menu.style.display === "block") {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "block";
+  }
+}
